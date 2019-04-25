7c366053c0c01e4e53fe9c201ee7fb5f03d595ff
DejaMeshae <daemeshae@yahoo.com>
2019-04-25 12:12:41 -0500

    TEST

---
+++ SuperHerosProject/Models/Superheros.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHerosProject.Models
+{
+    public class Superheros
+    {
+        public class SuperherosIn
+        {
+            [Key]
+
+            public int Id { get; set; } //represents Primary Key
+
+            public string name { get; set; }
+            public string alterEgo { get; set; }
+            public string primarySuperheroAbility { get; set; }
+            public string secondarySuperHeroAbility { get; set; }
+            public string catchPhrase { get; set; }
+
+        }
+    }
+
+}
+}
