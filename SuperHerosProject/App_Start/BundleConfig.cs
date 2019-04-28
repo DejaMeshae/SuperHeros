1b2cd5672f556c5ae0620bd10402c8a6e552e7f3
DejaMeshae <daemeshae@yahoo.com>
2019-04-28 00:11:40 -0500

    changed bootstrap theme and added scuba steve image

--- SuperHerosProject/App_Start/BundleConfig.cs
+++ SuperHerosProject/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace SuperHerosProject
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-readable.css",
+                      "~/Content/bootstrap-slate.css",
                       "~/Content/site.css"));
         }
     }
